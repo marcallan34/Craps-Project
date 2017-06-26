094ed54a1f3a3ad53286bd18b14e3dcc9a11e11f
Marc Smith <marc.allan34@gmail.com>
2017-06-25 22:05:19 -0500

    Update craps.js

--- craps.js
+++ craps.js
@@ -2,12 +2,14 @@ var bankroll = 100;
 
 var point = 0;
 
+//this is the function to roll while the point has not been set
 function rollNP() {
 	var rollNP = Math.floor(Math.random()*(12-1+1)+1);
 	point = rollNP;
 	return `the point is ${point}`;
 }
 
+//this is the function to roll while there is a set point
 function roll() {
 	var roll = Math.floor(Math.random()*(12-1+1)+1);
 		if(roll = point) {
