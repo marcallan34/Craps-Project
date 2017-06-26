f3e562fdc2b202d369c1d301e4abe1b5814da5d0
Marc Smith <marc.allan34@gmail.com>
2017-06-25 22:15:18 -0500

    Update craps.py

--- craps.py
+++ craps.py
@@ -5,7 +5,7 @@ import random
 #program begins
 dice_options = [2,3,3,4,4,4,5,5,5,5,6,6,6,6,6,7,7,7,7,7,7,8,8,8,8,8,9,9,9,9,10,10,10,11,11,12]
 bankroll = int(200)
-bet = int(5)
+pass_line_bet = int(5)
 point = []
 
 def comeout_roll():
